fe69324414ae3d7b900f3d96b62001ba0731e0dd
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2021-09-02 10:39:06 +0200

    Format lab1 code with scalafmt

--- lab1/preflow.scala
+++ lab1/preflow.scala
@@ -4,14 +4,14 @@ import java.io._
 import akka.actor._
 import akka.pattern.ask
 import akka.util.Timeout
-import scala.concurrent.{Await,ExecutionContext,Future,Promise}
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.io._
 
 case class Flow(f: Int)
 case class Debug(debug: Boolean)
-case class Control(control:ActorRef)
+case class Control(control: ActorRef)
 case class Source(n: Int)
 
 case object Print
@@ -22,149 +22,163 @@ case object Sink
 case object Hello
 
 class Edge(var u: ActorRef, var v: ActorRef, var c: Int) {
-	var	f = 0
+  var f = 0
 }
 
 class Node(val index: Int) extends Actor {
-	var	e = 0;				/* excess preflow. 						*/
-	var	h = 0;				/* height. 							*/
-	var	control:ActorRef = null		/* controller to report to when e is zero. 			*/
-	var	source:Boolean	= false		/* true if we are the source.					*/
-	var	sink:Boolean	= false		/* true if we are the sink.					*/
-	var	edge: List[Edge] = Nil		/* adjacency list with edge objects shared with other nodes.	*/
-	var	debug = false			/* to enable printing.						*/
-	
-	def min(a:Int, b:Int) : Int = { if (a < b) a else b }
+  var e = 0; /* excess preflow. 						*/
+  var h = 0; /* height. 							*/
+  var control: ActorRef = null /* controller to report to when e is zero. 			*/
+  var source: Boolean = false /* true if we are the source.					*/
+  var sink: Boolean = false /* true if we are the sink.					*/
+  var edge: List[Edge] =
+    Nil /* adjacency list with edge objects shared with other nodes.	*/
+  var debug = false /* to enable printing.						*/
 
-	def id: String = "@" + index;
+  def min(a: Int, b: Int): Int = { if (a < b) a else b }
 
-	def other(a:Edge, u:ActorRef) : ActorRef = { if (u == a.u) a.v else a.u }
+  def id: String = "@" + index;
 
-	def status: Unit = { if (debug) println(id + " e = " + e + ", h = " + h) }
+  def other(a: Edge, u: ActorRef): ActorRef = { if (u == a.u) a.v else a.u }
 
-	def enter(func: String): Unit = { if (debug) { println(id + " enters " + func); status } }
-	def exit(func: String): Unit = { if (debug) { println(id + " exits " + func); status } }
+  def status: Unit = { if (debug) println(id + " e = " + e + ", h = " + h) }
 
-	def relabel : Unit = {
+  def enter(func: String): Unit = {
+    if (debug) { println(id + " enters " + func); status }
+  }
+  def exit(func: String): Unit = {
+    if (debug) { println(id + " exits " + func); status }
+  }
 
-		enter("relabel")
+  def relabel: Unit = {
 
-		h += 1
+    enter("relabel")
 
-		exit("relabel")
-	}
+    h += 1
 
-	def receive = {
+    exit("relabel")
+  }
 
-	case Debug(debug: Boolean)	=> this.debug = debug
+  def receive = {
 
-	case Print => status
+    case Debug(debug: Boolean) => this.debug = debug
 
-	case Excess => { sender ! Flow(e) /* send our current excess preflow to actor that asked for it. */ }
+    case Print => status
 
-	case edge:Edge => { this.edge = edge :: this.edge /* put this edge first in the adjacency-list. */ }
+    case Excess => {
+      sender ! Flow(
+        e
+      ) /* send our current excess preflow to actor that asked for it. */
+    }
 
-	case Control(control:ActorRef)	=> this.control = control
+    case edge: Edge => {
+      this.edge =
+        edge :: this.edge /* put this edge first in the adjacency-list. */
+    }
 
-	case Sink	=> { sink = true }
+    case Control(control: ActorRef) => this.control = control
 
-	case Source(n:Int)	=> { h = n; source = true }
+    case Sink => { sink = true }
 
-	case _		=> {
-		println("" + index + " received an unknown message" + _) }
+    case Source(n: Int) => { h = n; source = true }
 
-		assert(false)
-	}
+    case _ =>
+      {
+        println("" + index + " received an unknown message" + _)
+      }
+
+      assert(false)
+  }
 
 }
 
+class Preflow extends Actor {
+  var s = 0; /* index of source node.					*/
+  var t = 0; /* index of sink node.					*/
+  var n = 0; /* number of vertices in the graph.				*/
+  var edge: Array[Edge] = null /* edges in the graph.						*/
+  var node: Array[ActorRef] = null /* vertices in the graph.					*/
+  var ret: ActorRef = null /* Actor to send result to.					*/
 
-class Preflow extends Actor
-{
-	var	s	= 0;			/* index of source node.					*/
-	var	t	= 0;			/* index of sink node.					*/
-	var	n	= 0;			/* number of vertices in the graph.				*/
-	var	edge:Array[Edge]	= null	/* edges in the graph.						*/
-	var	node:Array[ActorRef]	= null	/* vertices in the graph.					*/
-	var	ret:ActorRef 		= null	/* Actor to send result to.					*/
+  def receive = {
 
-	def receive = {
+    case node: Array[ActorRef] => {
+      this.node = node
+      n = node.size
+      s = 0
+      t = n - 1
+      for (u <- node)
+        u ! Control(self)
+    }
 
-	case node:Array[ActorRef]	=> {
-		this.node = node
-		n = node.size
-		s = 0
-		t = n-1
-		for (u <- node)
-			u ! Control(self)
-	}
+    case edge: Array[Edge] => this.edge = edge
 
-	case edge:Array[Edge] => this.edge = edge
+    case Flow(f: Int) => {
+      ret ! f /* somebody (hopefully the sink) told us its current excess preflow. */
+    }
 
-	case Flow(f:Int) => {
-		ret ! f			/* somebody (hopefully the sink) told us its current excess preflow. */
-	}
+    case Maxflow => {
+      ret = sender
 
-	case Maxflow => {
-		ret = sender
-
-		node(t) ! Excess	/* ask sink for its excess preflow (which certainly still is zero). */
-	}
-	}
+      node(
+        t
+      ) ! Excess /* ask sink for its excess preflow (which certainly still is zero). */
+    }
+  }
 }
 
 object main extends App {
-	implicit val t = Timeout(4 seconds);
+  implicit val t = Timeout(4 seconds);
 
-	val	begin = System.currentTimeMillis()
-	val system = ActorSystem("Main")
-	val control = system.actorOf(Props[Preflow], name = "control")
+  val begin = System.currentTimeMillis()
+  val system = ActorSystem("Main")
+  val control = system.actorOf(Props[Preflow], name = "control")
 
-	var	n = 0;
-	var	m = 0;
-	var	edge: Array[Edge] = null
-	var	node: Array[ActorRef] = null
+  var n = 0;
+  var m = 0;
+  var edge: Array[Edge] = null
+  var node: Array[ActorRef] = null
 
-	val	s = new Scanner(System.in);
+  val s = new Scanner(System.in);
 
-	n = s.nextInt
-	m = s.nextInt
+  n = s.nextInt
+  m = s.nextInt
 
-	/* next ignore c and p from 6railwayplanning */
-	s.nextInt
-	s.nextInt
+  /* next ignore c and p from 6railwayplanning */
+  s.nextInt
+  s.nextInt
 
-	node = new Array[ActorRef](n)
+  node = new Array[ActorRef](n)
 
-	for (i <- 0 to n-1)
-		node(i) = system.actorOf(Props(new Node(i)), name = "v" + i)
+  for (i <- 0 to n - 1)
+    node(i) = system.actorOf(Props(new Node(i)), name = "v" + i)
 
-	edge = new Array[Edge](m)
+  edge = new Array[Edge](m)
 
-	for (i <- 0 to m-1) {
+  for (i <- 0 to m - 1) {
 
-		val u = s.nextInt
-		val v = s.nextInt
-		val c = s.nextInt
+    val u = s.nextInt
+    val v = s.nextInt
+    val c = s.nextInt
 
-		edge(i) = new Edge(node(u), node(v), c)
+    edge(i) = new Edge(node(u), node(v), c)
 
-		node(u) ! edge(i)
-		node(v) ! edge(i)
-	}
+    node(u) ! edge(i)
+    node(v) ! edge(i)
+  }
 
-	control ! node
-	control ! edge
+  control ! node
+  control ! edge
 
-	val flow = control ? Maxflow
-	val f = Await.result(flow, t.duration)
+  val flow = control ? Maxflow
+  val f = Await.result(flow, t.duration)
 
-	println("f = " + f)
+  println("f = " + f)
 
-	system.stop(control);
-	system.terminate()
+  system.stop(control);
+  system.terminate()
 
-	val	end = System.currentTimeMillis()
+  val end = System.currentTimeMillis()
 
-	println("t = " + (end - begin) / 1000.0 + " s")
+  println("t = " + (end - begin) / 1000.0 + " s")
 }
