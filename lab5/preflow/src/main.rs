9b7493ed3c560ac35ef70a1e4898ad1134aea7f8
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2021-10-08 11:28:48 +0200

    5: Parallelise rust implementation

--- lab5/preflow/src/main.rs
+++ lab5/preflow/src/main.rs
@@ -1,12 +1,13 @@
 #[macro_use] extern crate text_io;
 
-use std::sync::{Mutex,Arc};
+use std::sync::{Mutex,Arc,Condvar};
 use std::collections::LinkedList;
 use std::cmp;
-//use std::thread;
+use std::thread;
+use std::process::exit;
 use std::collections::VecDeque;
 
-const DEBUG: bool = true;
+const DEBUG: bool = false;
 
 macro_rules! pr {
     ($fmt_string:expr, $($arg:expr),*) => {
@@ -23,7 +24,6 @@ struct Node {
 	i:	usize,			/* index of itself for debugging.	*/
 	e:	i32,			/* excess preflow.			*/
 	h:	i32,			/* height.				*/
-    in_excess : bool,
 }
 
 struct Edge {
@@ -33,13 +33,27 @@ struct Edge {
         c:      i32,
 }
 
+struct ExcessQueue {
+    queue: Mutex<VecDeque<usize>>,
+    cond: Condvar,
+}
+
+impl ExcessQueue {
+    fn new() -> ExcessQueue {
+        ExcessQueue {
+            queue: Mutex::new(VecDeque::new()),
+            cond: Condvar::new(),
+        }
+    }
+}
+
 impl Node {
 	fn new(ii:usize) -> Node {
-		Node { i: ii, e: 0, h: 0, in_excess: false }
+		Node { i: ii, e: 0, h: 0, }
 	}
 
 	fn new2(ii:usize, hh: i32) -> Node {
-		Node { i: ii, e: 0, h: hh, in_excess: false}
+		Node { i: ii, e: 0, h: hh, }
 	}
 }
 
@@ -49,20 +63,41 @@ impl Edge {
         }
 }
 
-fn enter_excess(excess: &mut VecDeque<usize>, u: &mut Node, n: usize){
-    if u.i != 0 && u.i != n-1 && u.e > 0 && !u.in_excess {
-        pr!("Adding node {} to excess", u.i);
-        excess.push_back(u.i);
+fn enter_excess(excess: &ExcessQueue, u: &mut Node, n: usize, index: usize){
+    pr!("Node {}: u.i != 0 && u.i != n-1 && u.e > 0 = {}, {}, {}"
+       , u.i, u.i != 0 , u.i != n-1 , u.e > 0);
+    if u.i != 0 && u.i != n-1 && u.e > 0 {
+        pr!("Thread {} adds node {} to excess", index, u.i);
+        excess.queue.lock().unwrap().push_back(u.i);
+        pr!("Added node to excess");
     }
-    u.in_excess = true;
+    excess.cond.notify_all();
 }
 
-fn leave_excess(excess: &mut VecDeque<usize>, nodes: &mut Vec<Arc<Mutex<Node>>>) -> usize {
-    let u = excess.pop_front().unwrap();
-    let mut u_node = nodes[u].lock().unwrap();
-    u_node.in_excess = false;
+fn leave_excess(excess: &ExcessQueue, nodes: &mut Vec<Arc<Mutex<Node>>>,  n: usize) -> usize {
+    pr!("Entering leave_excess");
+    let mut q = excess.queue.lock().unwrap();
+    let mut u_opt = q.pop_front();
+    while u_opt == None  {
+        are_we_done(&nodes, n);
+        q = excess.cond.wait(q).unwrap();
+        u_opt = q.pop_front();
+    }
+    let u = u_opt.unwrap();
     return u;
 }
+
+fn are_we_done(nodes: &Vec<Arc<Mutex<Node>>>, n: usize) -> bool {
+    let t_node = nodes[n-1].lock().unwrap();
+    let s_node = nodes[0].lock().unwrap();
+    pr!("are_we_done: t.e={}, sum={}", t_node.e, -s_node.e);
+    if t_node.e == -s_node.e {
+        println!("f = {}", t_node.e);
+        exit(0);
+    }
+    return t_node.e == -s_node.e;
+}
+
 
 fn main() {
 
@@ -72,9 +107,11 @@ fn main() {
 	let _p: usize = read!();	/* c and p are in the input from 6railwayplanning.	*/
 	let mut nodes = vec![];
 	let mut edges = vec![];
-	let mut adj: Vec<LinkedList<usize>> =Vec::with_capacity(n);
-	let mut excess: VecDeque<usize> = VecDeque::new();
+	let mut adj: Vec<LinkedList<usize>> = Vec::with_capacity(n);
+	//let mut excess: BlockingQueue<usize> = BlockingQueue::new();
+    let excess = Arc::new(ExcessQueue::new());
 	let debug = false;
+    let num_threads = 4;
 
 	let s = 0;
 	let t = n-1;
@@ -114,14 +151,6 @@ fn main() {
 			println!("");
 		}
 	}
-    //fn push(edge: Edge, u: Node) {
-    //}
-
-    //fn other(e: usize, u: usize) -> usize {
-    //    let edge = edges[e].lock().unwrap();
-    //    if edge.u == u {return edge.v;}
-    //    else {return edge.u;}
-    //}
 
 	pr!("initial pushes");
 	let iter = adj[s].iter();
@@ -149,75 +178,90 @@ fn main() {
         v_node.e += d;
 
         if v_node.e == d {
-            enter_excess(&mut excess, &mut v_node, n);
+            enter_excess(&excess, &mut v_node, n, 0);
         }
     }
 
 	// but nothing is done here yet...
 
-	while !excess.is_empty() {
-		//let mut c = 0;
-		let u = leave_excess(&mut excess, &mut nodes);
-        pr!("Take node {} from excess", u);
-        let iter = adj[u].iter();
+	let mut threads = vec![];
+	for i in 1 .. num_threads + 1{
+        let index = i;
+        let mut excess_l = excess.clone();
+        let mut nodes_l = nodes.clone();
+        let adj_l = adj.clone();
+        let edges_l = edges.clone();
+		let h = thread::spawn(move || {
+            loop {
+                pr!("Thread {} enters leave_excess", index);
+                let u = leave_excess(&mut excess_l, &mut nodes_l, n);
+                pr!("Thread {} takes node {} from excess", index, u);
+                let iter = adj_l[u].iter();
 
-        {
-            let u_node = nodes[u].lock().unwrap();
-            pr!("with excess {}", u_node.e);
-        }
+                {
+                    let u_node = nodes_l[u].lock().unwrap();
+                    pr!("with excess {}", u_node.e);
+                }
 
-        let mut has_pushed = false;
-        for e in iter {
-            let v : usize;
-            let mut edge = edges[*e].lock().unwrap();
-            if edge.u == u {v = edge.v;}
-            else {v = edge.u;}
+                let mut has_pushed = false;
+                for e in iter {
+                    let v : usize;
+                    let mut edge = edges_l[*e].lock().unwrap();
+                    if edge.u == u {v = edge.v;}
+                    else {v = edge.u;}
 
-            let mut u_node = nodes[u].lock().unwrap();
-            if u_node.e == 0 {break;}
-            //pr!("with excess {}", u_node.e);
-            let mut v_node = nodes[v].lock().unwrap();
-            if u_node.h > v_node.h {
-                let d : i32;
-                if u == edge.u {
-                    d = cmp::min(u_node.e, edge.c - edge.f);
-                    edge.f += d;
+                    let mut u_node;
+                    let mut v_node;
+                    if u < v {
+                        u_node = nodes_l[u].lock().unwrap();
+                        v_node = nodes_l[v].lock().unwrap();
+                    } else {
+                        v_node = nodes_l[v].lock().unwrap();
+                        u_node = nodes_l[u].lock().unwrap();
+                    }
+                    if u_node.e == 0 {break;}
+                    if u_node.h > v_node.h {
+                        let d : i32;
+                        if u == edge.u {
+                            d = cmp::min(u_node.e, edge.c - edge.f);
+                            edge.f += d;
+                        } else {
+                            d = cmp::min(u_node.e, edge.c + edge.f);
+                            edge.f -= d;
+                        }
+                        if d > 0 {
+                            pr!("Pushing: {}->{}: d={}", u, v, d);
+                            u_node.e -= d;
+                            v_node.e += d;
+
+                            if v_node.e == d {
+                                enter_excess(&excess_l, &mut v_node, n, index);
+                            }
+                            has_pushed = true;
+                        }
+                    }
+                }
+
+                if !has_pushed {
+                    let mut u_node = nodes_l[u].lock().unwrap();
+                    u_node.h += 1;
+                    pr!("Relabeled node {}, new h={}", u, u_node.h);
+                    enter_excess(&excess_l, &mut u_node, n, index);
                 } else {
-                    d = cmp::min(u_node.e, edge.c + edge.f);
-                    edge.f -= d;
-                }
-                if d > 0 {
-                    pr!("Pushing: {}->{}: d={}", u, v, d);
-                    u_node.e -= d;
-                    v_node.e += d;
-
-                    //if u_node.e > 0 {
-                        //excess.push_back(u);
-                    //}
-                    //if v_node.e == d {
-                        enter_excess(&mut excess, &mut v_node, n);
-                        //excess.push_back(v);
-                    //}
-                    has_pushed = true;
+                    let mut u_node = nodes_l[u].lock().unwrap();
+                    if u_node.e > 0 {
+                        enter_excess(&excess_l, &mut u_node, n, index);
+                    }
                 }
             }
-        }
-        {
-            let mut u_node = nodes[u].lock().unwrap();
-            if u_node.e > 0 {
-                enter_excess(&mut excess, &mut u_node, n);
-            }
-        }
+            //println!("Thread {} exits", index);
+        });
+        threads.push(h);
+    }
 
-
-        if !has_pushed {
-            let mut u_node = nodes[u].lock().unwrap();
-            u_node.h += 1;
-            pr!("Relabeled node {}, new h={}", u, u_node.h);
-            enter_excess(&mut excess, &mut u_node, n);
-            //excess.push_back(u);
-        }
-	}
+    for h in threads {
+        h.join().unwrap();
+    }
 
     let t_node = nodes[t].lock().unwrap();
 	println!("f = {}", t_node.e);
