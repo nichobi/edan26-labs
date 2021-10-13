4d5106a166e397eb001cb79275f411deb8c8903d
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2021-10-13 09:42:25 +0200

    6: mmp.c: Transpose c matrix to avoid cache misses

--- lab6/mmp.c
+++ lab6/mmp.c
@@ -18,7 +18,7 @@ void matmul()
 		for (j = 0; j < N; j += 1) {
 			a[i][j] = 0;
 			for (k = 0; k < N; k += 1) {
-				a[i][j] += b[i][k] * c[k][j];
+				a[i][j] += b[i][k] * c[j][k];
 			}
 		}
 	}
@@ -32,7 +32,7 @@ void init()
 	for (i = 0; i < N; i += 1) {
 		for (j = 0; j < N; j += 1) {
 			b[i][j] = 12 + i * j * 13;
-			c[i][j] = -13 + i + j * 21;
+			c[j][i] = -13 + i + j * 21;
 		}
 	}
 }
